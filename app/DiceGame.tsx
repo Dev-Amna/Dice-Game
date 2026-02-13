1f3fe0047da16eb72f466bf7835275037a093548
Amna Ashraf <amnaharaf272@gmail.com>
2026-02-13 13:17:36 +0500

    Generate Random Dice

--- app/DiceGame.tsx
+++ app/DiceGame.tsx
@@ -1,16 +1,102 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { useLocalSearchParams } from 'expo-router';
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const DiceGame = () => {
+  const { initialPoint } = useLocalSearchParams();
+
+  const diceList = [
+    require("../assets/images/d1.png"),
+    require("../assets/images/d2.png"),
+    require("../assets/images/d3.png"),
+    require("../assets/images/d4.png"),
+    require("../assets/images/d5.png"),
+    require("../assets/images/d6.png"),
+  ]
+
+  const sound = {
+    win: require("../assets/audio/levelup.mp3"),
+    lose: require("../assets/audio/wrong.mp3"),
+  }
+
+
+  const [index1, setIndex1] = useState(0);
+  const [index2, setIndex2] = useState(0);
+  const [sum, setSum] = useState(0);
+
+
+  function generateRandomNumber() {
+    return Math.floor(Math.random() * 6);
+  }
+
+  function rollTheDice() {
+    setIndex1(generateRandomNumber());
+    setIndex2(generateRandomNumber());
+  }
   return (
     <View style={styles.container}>
-      <Text>DiceGame</Text>
+      <View style={styles.diceRow}>
+        <Image source={diceList[index1]} style={styles.dice} />
+        <Image source={diceList[index2]} style={styles.dice} />
+      </View>
+
+      <View style={styles.scroborad}>
+        <Text style={styles.pointText}>Your Point : {initialPoint}</Text>
+        <Text style={styles.sumText}>Sum Text  : {sum}</Text>
+      </View>
+
+      <TouchableOpacity style={styles.btn} onPress={rollTheDice} >
+        <Text style={styles.btnText}>
+          ROLL
+        </Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
+
 export default DiceGame
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 20
+  },
+  diceRow: {
+    flexDirection: "row",
+    justifyContent: "center",
+
+  },
+  dice: {
+    width: 100,
+    height: 100,
+    borderRadius: 12,
+    margin: 12
+  },
+  btn: {
+    backgroundColor: "black",
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: "black",
+  },
+  btnText: {
+    color: "white",
+    paddingVertical: 10,
+    marginHorizontal: 40
+  },
+  pointText: {
+    fontSize: 18,
+    color: "gray"
+  },
+  sumText: {
+    fontSize: 20,
+    color: "black",
+    fontWeight: "500"
+  },
+  scroborad: {
+    justifyContent: "center",
+    alignItems: "center"
+  }
 })
--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,40 @@
 
-import { router } from "expo-router";
+import { useRouter } from "expo-router";
 import { useState } from "react";
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function Home() {
-  const moveNext = () => {
-    router.push("/DiceGame");
-  }
+  let router = useRouter();
 
-  const [initialPoint, setInitialPoint] = useState("");
+  const [initialPoint, setInitialPoint] = useState("999");
   const [error, setError] = useState("");
 
 
-  // const vaild = () => {
-  //   const
-  // }
+  function vaildateInput(): any {
+    const input = Number.parseInt(initialPoint);
+
+    if (isNaN(input) || input < 100 || input > 999) {
+      setError("Please enter a valid point between 100 andd 999");
+      return false;
+    }
+    setError("");
+    return true;
+  }
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Enter Initial Point!</Text>
-      <TextInput style={styles.textInput} placeholder="min 100 max 999" keyboardType="numeric" maxLength={3} onChangeText={setInitialPoint} />
-     <Text style={styles.errText}>{error}</Text>
-      <TouchableOpacity onPress={() => { }} style={styles.button}>
+      <TextInput style={styles.textInput} defaultValue={initialPoint} placeholder="min 100 max 999" keyboardType="numeric" maxLength={3} onChangeText={setInitialPoint} />
+      {error ? <Text style={styles.errText}>{error}</Text> : null}
+      <TouchableOpacity onPress={() => {
+        if (vaildateInput()) {
+          router.push({
+            pathname: "/DiceGame",
+            params: { initialPoint: initialPoint }
+          });
+        }
+      }} style={styles.button}>
         <Text style={styles.btnTxt}>Start</Text>
       </TouchableOpacity>
     </View>
@@ -50,15 +64,17 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 10,
     borderColor: "black",
-   
-   
+
+
   },
   btnTxt: {
-    color : "white",
-      paddingVertical: 10,
+    color: "white",
+    paddingVertical: 10,
     marginHorizontal: 40
+  },
+
+  errText: {
+    color: "red",
+    margin: 5
   }
-,errText :{
-  
-}
 })
